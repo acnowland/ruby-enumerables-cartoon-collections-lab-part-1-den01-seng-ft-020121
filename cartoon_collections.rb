41161a9b908aa15a2fe189b6595d3b444edb6848
Adam Nowland <acnowland@gmail.com>
2020-12-28 21:19:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,8 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |character|
-    puts 'Hello ' + character
+    puts "Hello #{character}!"
+  end
 end
 
 def list_dwarves(array)
